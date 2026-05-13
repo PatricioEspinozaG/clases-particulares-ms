3950b6438c2923f48e9f626250494929ebcd5766
Patricio Espinoza <patri.espinozag@duocuc.cl>
2026-05-12 23:45:26 -0400

    feat: crear entidad usuario y repository

---
+++ auth-service/src/main/java/com/classmate/authservice/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.classmate.authservice.repository;
+
+import com.classmate.authservice.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+}
