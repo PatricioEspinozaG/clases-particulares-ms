f9361a49f58d0700f9577c6468c9deefc6c75f23
José <jc.ramos@duocuc.cl>
2026-05-13 18:43:01 -0400

    feat:entidad clase 1.1

--- clase-service/src/main/java/com/classmate/claseservice/controller/Clase.java
+++ clase-service/src/main/java/com/classmate/claseservice/controller/Clase.java
@@ -1,35 +1,30 @@
-package com.classmate.claseservice.controller;
+package com.classmate.reservaservice.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
+@Table(name = "clases")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Clase {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull(message = "La id del profesor es obligatoria")
-    private Long profesorId;
+    @Column(nullable = false)
+    private String nombre;
 
-    @NotBlank(message = "El título es obligatorio")
-    private String titulo;
-
-    @NotBlank(message = "La descripción es obligatoria")
+    @Column(nullable = false)
     private String descripcion;
 
-    @NotNull(message = "La duración es obligatoria")
-    @Min(value = 1, message = "La duración debe ser mayor a 0")
-    @Max(value = 90, message = "La duración no debe ser mayor a 90 mins")
-    private Integer duracion;
+    @Column(nullable = false)
+    private Double precio;
+
+    @Column(nullable = false)
+    private Integer duracionMinutos;
 }
