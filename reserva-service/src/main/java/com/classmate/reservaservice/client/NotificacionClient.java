dd730229b71cf069753ab30d09b8a7b89f2aae3e
Patricio Espinoza <patri.espinozag@duocuc.cl>
2026-06-17 23:39:28 -0400

    refactor: integrate OpenFeign with Eureka service discovery

--- reserva-service/src/main/java/com/classmate/reservaservice/client/NotificacionClient.java
+++ reserva-service/src/main/java/com/classmate/reservaservice/client/NotificacionClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "notification-service", url = "http://notificacion-service:8087")
+@FeignClient(name = "notificacion-service")
 public interface NotificacionClient {
 
     @PostMapping("/notificaciones/reserva")
