aeab4195e1232b3806f98dfc22a86c653aa17b66
José <jc.ramos@duocuc.cl>
2026-06-20 14:41:48 -0400

    test: add jacoco datafaker and hateoas to clase service

--- clase-service/src/main/java/com/classmate/claseservice/controller/ClaseController.java
+++ clase-service/src/main/java/com/classmate/claseservice/controller/ClaseController.java
@@ -1,15 +1,19 @@
 package com.classmate.claseservice.controller;
 
-
 import com.classmate.claseservice.dto.ClaseRequest;
 import com.classmate.claseservice.dto.ClaseResponse;
 import com.classmate.claseservice.service.ClaseService;
 import jakarta.validation.Valid;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/clases")
@@ -17,46 +21,77 @@ public class ClaseController {
 
     private final ClaseService claseService;
 
-    public ClaseController(ClaseService claseService){
+    public ClaseController(ClaseService claseService) {
         this.claseService = claseService;
     }
 
     @PostMapping
-    public ResponseEntity<ClaseResponse> crearClase(
-            @Valid @RequestBody ClaseRequest request){
+    public ResponseEntity<EntityModel<ClaseResponse>> crearClase(
+            @Valid @RequestBody ClaseRequest request) {
+
         ClaseResponse response = claseService.crearClase(request);
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(response);
+                .body(agregarLinks(response));
     }
 
     @GetMapping
-    public ResponseEntity<List<ClaseResponse>> obtenerClases(){
-        return ResponseEntity.ok(claseService.obtenerClases());
+    public ResponseEntity<CollectionModel<EntityModel<ClaseResponse>>> obtenerClases() {
+
+        List<EntityModel<ClaseResponse>> clases = claseService.obtenerClases()
+                .stream()
+                .map(this::agregarLinks)
+                .toList();
+
+        CollectionModel<EntityModel<ClaseResponse>> collection = CollectionModel.of(clases);
+
+        collection.add(linkTo(methodOn(ClaseController.class)
+                .obtenerClases()).withSelfRel());
+
+        return ResponseEntity.ok(collection);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ClaseResponse> actualizarClase(
-            @PathVariable Long id, @Valid @RequestBody ClaseRequest request){
+    public ResponseEntity<EntityModel<ClaseResponse>> actualizarClase(
+            @PathVariable Long id,
+            @Valid @RequestBody ClaseRequest request) {
 
-        ClaseResponse  claseActualizada = claseService.actualizarClase(id, request);
+        ClaseResponse claseActualizada = claseService.actualizarClase(id, request);
 
-        return ResponseEntity.ok(claseActualizada);
+        return ResponseEntity.ok(agregarLinks(claseActualizada));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ClaseResponse> obtenerClasePorId(
-            @PathVariable Long id){
+    public ResponseEntity<EntityModel<ClaseResponse>> obtenerClasePorId(
+            @PathVariable Long id) {
 
-        return ResponseEntity.ok(claseService.obtenerClasePorId(id));
+        ClaseResponse response = claseService.obtenerClasePorId(id);
+
+        return ResponseEntity.ok(agregarLinks(response));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> eliminarClase(@PathVariable Long id){
+    public ResponseEntity<String> eliminarClase(@PathVariable Long id) {
 
         claseService.eliminarClase(id);
 
         return ResponseEntity.ok("Clase eliminada correctamente");
+    }
+
+    private EntityModel<ClaseResponse> agregarLinks(ClaseResponse response) {
+
+        EntityModel<ClaseResponse> resource = EntityModel.of(response);
+
+        resource.add(linkTo(methodOn(ClaseController.class)
+                .obtenerClasePorId(response.getId())).withSelfRel());
+
+        resource.add(linkTo(methodOn(ClaseController.class)
+                .obtenerClases()).withRel("clases"));
+
+        resource.add(linkTo(methodOn(ClaseController.class)
+                .eliminarClase(response.getId())).withRel("eliminar"));
+
+        return resource;
     }
 }
